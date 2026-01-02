5d00f55043b8176c929fbaa2c09fc9c6689b16d6
Ch4120N <charonsecagency@gmail.com>
2026-01-02 00:57:12 -0800

    added InfoMessage function

--- src/include/decorators.h
+++ src/include/decorators.h
@@ -9,3 +9,8 @@ std::string PassMessage(const std::string RequestMessage, Color color = Color::F
 {
     return color_code(Color::FG_BRIGHT_GREEN) + " [ " + color_code(Color::FG_BRIGHT_WHITE) + "+" + color_code(Color::FG_BRIGHT_GREEN) + " ] " + color_code(color) + RequestMessage + color_code(Color::RESET);
 }
+
+std::string InfoMessage(const std::string RequestMessage, Color color = Color::FG_WHITE)
+{
+    return color_code(Color::FG_BRIGHT_CYAN) + " [ " + color_code(Color::FG_BRIGHT_WHITE) + "*" + color_code(Color::FG_BRIGHT_CYAN) + " ] " + color_code(color) + RequestMessage + color_code(Color::RESET);
+}
