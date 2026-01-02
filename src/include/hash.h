c5b3220c6e6bb0b38153b213899ec63f1576a683
Ch4120N <charonsecagency@gmail.com>
2026-01-02 00:39:20 -0800

    Create the simple private functions

--- src/include/hash.h
+++ src/include/hash.h
@@ -15,4 +15,22 @@ private:
 
     uint32 h0, h1, h2, h3;
 
+    static uint32 leftRotate(uint32 x, uint32 c)
+    {
+        return (x << c) | (x >> (32 - c));
+    }
+
+    static void toBytes(uint32 val, uint8 *bytes)
+    {
+        bytes[0] = (uint8)val;
+        bytes[1] = (uint8)(val >> 8);
+        bytes[2] = (uint8)(val >> 16);
+        bytes[3] = (uint8)(val >> 24);
+    }
+
+    static uint32 toInt32(const uint8 *bytes)
+    {
+        return (uint32)bytes[0] | ((uint32)bytes[1] << 8) | ((uint32)bytes[2] << 16) | ((uint32)bytes[3] << 24);
+    }
+
 };
