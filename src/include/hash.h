469693c9e8bb56d0daa4908a3439fb4ff35fc8e7
Ch4120N <charonsecagency@gmail.com>
2026-01-02 00:38:30 -0800

    Create private required variable

--- src/include/hash.h
+++ src/include/hash.h
@@ -5,5 +5,14 @@
 #include <iomanip>
 
 class MD5{
+private:
+    typedef uint8_t uint8;
+    typedef uint32_t uint32;
+    typedef uint64_t uint64;
+
+    const static uint32 s[64];
+    const static uint32 K[64];
+
+    uint32 h0, h1, h2, h3;
 
 };
