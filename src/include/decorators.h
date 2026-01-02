217a02be6c347a0a7239ca05c0ece4d9ca4ec9e5
Ch4120N <charonsecagency@gmail.com>
2026-01-02 01:51:30 -0800

    Update decorators.h

--- src/include/decorators.h
+++ src/include/decorators.h
@@ -1,36 +1,36 @@
 #include "color.h"
 
-std::string FailMessage(const std::string RequestMessage, Color color = Color::FG_WHITE)
+std::string FailMessage(const std::string RequestMessage, Color color = Color::FG_BRIGHT_WHITE)
 {
     return color_red() + " [ " + color_bwhite() + "-" + color_red() + " ] " + color_code(color) + RequestMessage + reset();
 }
 
-std::string PassMessage(const std::string RequestMessage, Color color = Color::FG_WHITE)
+std::string PassMessage(const std::string RequestMessage, Color color = Color::FG_BRIGHT_WHITE)
 {
     return color_green() + " [ " + color_bwhite() + "+" + color_green() + " ] " + color_code(color) + RequestMessage + reset();
 }
 
-std::string InfoMessage(const std::string RequestMessage, Color color = Color::FG_WHITE)
+std::string InfoMessage(const std::string RequestMessage, Color color = Color::FG_BRIGHT_WHITE)
 {
     return color_cyan() + " [ " + color_bwhite() + "*" + color_cyan() + " ] " + color_code(color) + RequestMessage + reset();
 }
 
-std::string WarningMessage(const std::string RequestMessage, Color color = Color::FG_WHITE)
+std::string WarningMessage(const std::string RequestMessage, Color color = Color::FG_BRIGHT_WHITE)
 {
     return color_orange() + " [ " + color_bwhite() + "!" + color_orange() + " ] " + color_code(color) + RequestMessage + reset();
 }
 
-std::string DoubleWarningMessage(const std::string RequestMessage, Color color = Color::FG_WHITE)
+std::string DoubleWarningMessage(const std::string RequestMessage, Color color = Color::FG_BRIGHT_WHITE)
 {
     return color_orange() + " [ " + color_bwhite() + "!" + color_orange() + " ] " + color_code(color) + RequestMessage + color_orange() + " [ " + color_bwhite() + "!" + color_orange() + " ] " + reset();
 }
 
-std::string TipMessage(const std::string RequestMessage, Color color = Color::FG_WHITE)
+std::string TipMessage(const std::string RequestMessage, Color color = Color::FG_BRIGHT_WHITE)
 {
     return color_magenta() + " [ " + color_bwhite() + "!" + color_magenta() + " ] " + color_code(color) + RequestMessage + reset();
 }
 
-std::string ProgressMessage(const std::string RequestMessage, Color color = Color::FG_WHITE)
+std::string ProgressMessage(const std::string RequestMessage, Color color = Color::FG_BRIGHT_WHITE)
 {
     return color_magenta() + "\r [ " + color_bwhite() + ">" + color_magenta() + " ] " + color_code(color) + RequestMessage + reset();
 }
