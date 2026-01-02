244064d7b672d80fa2432a061f402ec46c83f7ef
Ch4120N <charonsecagency@gmail.com>
2026-01-02 00:46:30 -0800

    function for processing blocks of hash

--- src/include/hash.h
+++ src/include/hash.h
@@ -33,4 +33,55 @@ private:
         return (uint32)bytes[0] | ((uint32)bytes[1] << 8) | ((uint32)bytes[2] << 16) | ((uint32)bytes[3] << 24);
     }
 
+    void processBlock(const uint8 *block)
+    {
+        uint32 a = h0;
+        uint32 b = h1;
+        uint32 c = h2;
+        uint32 d = h3;
+        uint32 f, g, temp;
+
+        uint32 M[16];
+        for (int i = 0; i < 16; i++)
+        {
+            M[i] = toInt32(block + i * 4);
+        }
+
+        // Main loop
+        for (uint32 i = 0; i < 64; i++)
+        {
+            if (i < 16)
+            {
+                f = (b & c) | ((~b) & d);
+                g = i;
+            }
+            else if (i < 32)
+            {
+                f = (d & b) | ((~d) & c);
+                g = (5 * i + 1) % 16;
+            }
+            else if (i < 48)
+            {
+                f = b ^ c ^ d;
+                g = (3 * i + 5) % 16;
+            }
+            else
+            {
+                f = c ^ (b | (~d));
+                g = (7 * i) % 16;
+            }
+
+            temp = d;
+            d = c;
+            c = b;
+            b = b + leftRotate((a + f + K[i] + M[g]), s[i]);
+            a = temp;
+        }
+
+        h0 += a;
+        h1 += b;
+        h2 += c;
+        h3 += d;
+    }
+
 };
