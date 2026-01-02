12052e557a00e384829e0a4b8de1113a956d2efe
Ch4120N <charonsecagency@gmail.com>
2026-01-02 00:58:00 -0800

    added DoubleWarningMessage function

--- src/include/decorators.h
+++ src/include/decorators.h
@@ -19,3 +19,8 @@ std::string WarningMessage(const std::string RequestMessage, Color color = Color
 {
     return color_code(Color::FG_YELLOW) + " [ " + color_code(Color::FG_BRIGHT_WHITE) + "!" + color_code(Color::FG_YELLOW) + " ] " + color_code(color) + RequestMessage + color_code(Color::RESET);
 }
+
+std::string DoubleWarningMessage(const std::string RequestMessage, Color color = Color::FG_WHITE)
+{
+    return color_code(Color::FG_YELLOW) + " [ " + color_code(Color::FG_BRIGHT_WHITE) + "!" + color_code(Color::FG_YELLOW) + " ] " + color_code(color) + RequestMessage + color_code(Color::FG_YELLOW) + " [ " + color_code(Color::FG_BRIGHT_WHITE) + "!" + color_code(Color::FG_YELLOW) + " ] " + color_code(Color::RESET);
+}
